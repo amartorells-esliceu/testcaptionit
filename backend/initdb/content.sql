3f8795478eab47be7c45c3c79289bb63d35ef3c6
Andreu <amartorells@esliceu.net>
2026-05-09 10:00:18 +0200

    exemple de contingut

---
+++ backend/initdb/content.sql
@@ -0,0 +1,11 @@
+-- EXEMPLES D'IMATGES (modality_id = 1)
+INSERT INTO templates (content, modality_id) VALUES 
+('https://api.memegen.link/images/doge.png', 1),
+('https://api.memegen.link/images/fine.png', 1),
+('https://api.memegen.link/images/pika.png', 1),
+('https://api.memegen.link/images/buzz.png', 1),
+('https://api.memegen.link/images/drake.png', 1);
+
+-- EXEMPLES DE FRASES (modality_id = 2)
+INSERT INTO templates (content, modality_id) VALUES 
+('L''invent més inútil del segle XXI ha estat...', 2);
