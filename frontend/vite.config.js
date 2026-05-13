55355209ad089532dfd0dc9df172a9c41928dafb
Andreu <amartorells@esliceu.net>
2026-05-13 20:12:57 +0200

    fitx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,26 +2,26 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'frontend',
+  root: '.', 
   
   build: {
     rollupOptions: {
       input: {
-        main: resolve(import.meta.dirname, 'frontend/index.html'),
-        
-        round: resolve(import.meta.dirname, 'frontend/round/index.html'),
-        ranking: resolve(import.meta.dirname, 'frontend/ranking/index.html'),
-        podium: resolve(import.meta.dirname, 'frontend/podium/index.html'),
-        room: resolve(import.meta.dirname, 'frontend/room/index.html'),
-        configureRoom: resolve(import.meta.dirname, 'frontend/configureRoom/index.html'),
-        createOrJoinRoom: resolve(import.meta.dirname, 'frontend/createOrJoinRoom/index.html'),
-        ansguersVotes: resolve(import.meta.dirname, 'frontend/ansguersVotes/index.html'),
-        resetGame: resolve(import.meta.dirname, 'frontend/resetGame/index.html'),
+        main: resolve(import.meta.dirname, 'index.html'),
+        round: resolve(import.meta.dirname, 'round/index.html'),
+        ranking: resolve(import.meta.dirname, 'ranking/index.html'),
+        podium: resolve(import.meta.dirname, 'podium/index.html'),
+        room: resolve(import.meta.dirname, 'room/index.html'),
+        configureRoom: resolve(import.meta.dirname, 'configureRoom/index.html'),
+        createOrJoinRoom: resolve(import.meta.dirname, 'createOrJoinRoom/index.html'),
+        ansguersVotes: resolve(import.meta.dirname, 'ansguersVotes/index.html'),
+        resetGame: resolve(import.meta.dirname, 'resetGame/index.html'),
       },
     },
   },
 
   server: {
+    host: true,
     fs: {
       allow: ['..']
     }
