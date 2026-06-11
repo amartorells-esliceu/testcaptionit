e608d1726145a9578edee6816d2513e66fddb84b
Joan <jllinass@esliceu.net>
2026-06-11 17:34:03 +0200

    canvis a vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: '.', 
+  root: './', 
   
   plugins: [
     tailwindcss(),
